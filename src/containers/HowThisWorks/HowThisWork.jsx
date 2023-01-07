56754586c856b8cf5af5067c3f1e312eb51d165f
Israel Kenneth <israel.kenneth2011@gmal.com>
2023-01-07 01:40:34 +0100

    schon fertig

--- src/containers/HowThisWorks/HowThisWork.jsx
+++ src/containers/HowThisWorks/HowThisWork.jsx
@@ -4,21 +4,20 @@ import "./howThisWork.css"
 
 const HowThisWork = () => (
 
-    <div className="gallie__HowThisWork section__margin1">
+    <div className="gallie__HowThisWork section__margin">
         <div className="gallie__HowThisWork-heading">
-            <h1 className="gradient__text">How Does This Work</h1>
+            <h2>How Does This Work?</h2>
             <p>Follow the process below</p>
         </div>
         <div className='gallie__HowThisWork-full'>
         <div className="gallie__HowThisWork-container">
-                <h1 className="gradient__text gallie__HowThisWork-head-text">Looking for animal care?</h1>
+            <h3 className="gradient__text gallie__HowThisWork-head-text">Looking for animal care?</h3>
             <Feature title="Sign up now" text="You will have access to our database of pet sitters after you register." />
             <Feature title="Create Your Job Offer" text="After that, local pet sitters who are interested in your employment will submit applications to you." />
             <Feature title="Look up animal care" text="Utilize the contact form to locate and get in touch with reliable dog, cat, and other sitters in your region." />
         </div>
-
         <div className="gallie__HowThisWork-container">
-                <h1 className="gradient__text gallie__HowThisWork-head-text">Want a pet sitter</h1>
+                <h3 className="gradient__text gallie__HowThisWork-head-text">Want a pet sitter?</h3>
                 <Feature title="Sign up now" text="You will have access to job postings for dog walkers and pet caregivers after you register." />
                 <Feature title="Create Your Profile" text="Include as much information as you can about the animal care you intend to provide in your personal profile." />
                 <Feature title="Apply for Jobs" text="Find suitable dog walking or pet sitting positions in your area and submit your application straight using the contact form." />
--- src/containers/Services/Services.jsx
+++ src/containers/Services/Services.jsx
@@ -7,7 +7,7 @@ import Errands from "../../assets/Services/Errands.png"
 
 const Services = () => {
     return (
-        <div className="gallie__Services section__margin1">
+        <div className="gallie__Services section__padding">
             <div className="gallie__Services-heading">
                 <h2 className="gradient__text">Services at Gallie</h2>
             </div>
@@ -21,16 +21,17 @@ const Services = () => {
                         <p>Your pets spend the night in the house of your pet sitter. They will get a warm welcome and be treated as members of the family.</p>
                     </div>
                 </div>
-                <div className='gallie__Services-Body-Content'>
-
-                    <div className='gallie__Services-Body-Content__text'>
-                        <h2>House Sitting </h2>
-                        <p>Your pets and home are taken care of by our sitter. Without leaving the house, your pets will receive all the attention they require.</p>
-                    </div>
+                <div className='gallie__Services-Body-Content2'>
                     <div className='gallie__Services-Body-Content__image'>
                         <img src={HouseSitin} alt='img' />
                     </div>
+                    <div className='gallie__Services-Body-Content__text2'>
+                        <h2>House Sitting</h2>
+                        <p>Your pets and home are taken care of by our sitter. Without leaving the house, your pets will receive all the attention they require.</p>
+                    </div>
                 </div>
+
+
                 <div className='gallie__Services-Body-Content'>
                     <div className='gallie__Services-Body-Content__image'>
                         <img src={Walking} alt='img' />
@@ -40,15 +41,19 @@ const Services = () => {
                         <p>Your dog is taken for a stroll through the neighborhood. Ideal for dogs with excess energy and active days.</p>
                     </div>
                 </div>
-                <div className='gallie__Services-Body-Content'>
 
-                    <div className='gallie__Services-Body-Content__text'>
-                        <h2>Running Errands </h2>
-                        <p>We go on short trips to deliver or collect something, on your behalf.</p>
-                    </div>
+
+
+
+                <div className='gallie__Services-Body-Content2'>
                     <div className='gallie__Services-Body-Content__image'>
                         <img src={Errands} alt='img' />
                     </div>
+                    <div className='gallie__Services-Body-Content__text2'>
+                        <h2>Running Errands </h2>
+                        <p>We go on short trips to deliver or collect something, on your behalf.</p>
+                    </div>
+
                 </div>
 
             </div>
